551e1148162ab8747953b9c0e4d4ae2ee746445f
TamaraRibeiro <tamara.fl@hotmail.com>
2025-02-23 11:41:38 -0300

    :art: add animated arrow in card-list

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { CharacterProps, Species } from "../@types/types";
 import axios from "axios";
 import ModalCardInfo from "./modal-card-info";
+import { TiArrowRightOutline } from "react-icons/ti";
 
 export default function CardList({
   pokemonCharacter,
@@ -28,21 +29,29 @@ export default function CardList({
     <>
       <div
         onClick={handleOpenModal}
-        className="border border-zinc-900 bg-nile-blue-800 px-6 w-full max-w-md lg:max-w-3xl rounded-lg shadow-shape cursor-pointer hover:-translate-y-[1px] hover:translate-x-[1.8px]  transition ease-in-out duration-300"
+        className="border border-zinc-900 bg-nile-blue-800 px-6 w-full max-w-md lg:max-w-3xl rounded-lg shadow-shape cursor-pointer hover:-translate-y-[1px] hover:translate-x-[1.8px]  transition ease-in-out duration-300 group"
       >
         <div className="flex items-center gap-5 py-2.5">
-          <div className="flex items-center justify-center h-24 px-3 border border-mint-600 rounded-xl shadow-shape ">
+          <div className="flex items-center justify-center h-24 px-3 border border-mint-600 rounded-xl shadow-shape min-w-30">
             <img
               src={pokemonData && pokemonData.sprites.front_default}
               alt="img"
             />
           </div>
-          <span className="font-semibold tracking-wide text-lg lg:text-2xl capitalize">
-            {pokemonCharacter.name}
-          </span>
+          <div className="flex flex-col gap-4 lg:flex-row items-center justify-between w-full">
+            <span className="font-semibold tracking-wide text-lg lg:text-2xl capitalize">
+              {pokemonCharacter.name}
+            </span>
+            <div className="flex items-end"><TiArrowRightOutline size={22} className="group-hover:scale-110 group-hover:text-mint-300 transition ease-in-out duration-300 group-hover:opacity-0 group-hover:translate-x-4" /></div>
+          </div>
         </div>
       </div>
-      {isModalOpen && <ModalCardInfo handleOpenModal={handleOpenModal} pokemonInfo={pokemonData!} />}
+      {isModalOpen && (
+        <ModalCardInfo
+          handleOpenModal={handleOpenModal}
+          pokemonInfo={pokemonData!}
+        />
+      )}
     </>
   );
 }
