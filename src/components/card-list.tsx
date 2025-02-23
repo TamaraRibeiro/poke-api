9b018885399d598d017da3e5f7e0d16adb76f237
TamaraRibeiro <tamara.fl@hotmail.com>
2025-02-23 11:44:32 -0300

    :hammer: fix card-list padding

--- src/components/card-list.tsx
+++ src/components/card-list.tsx
@@ -29,7 +29,7 @@ export default function CardList({
     <>
       <div
         onClick={handleOpenModal}
-        className="border border-zinc-900 bg-nile-blue-800 px-6 w-full max-w-md lg:max-w-3xl rounded-lg shadow-shape cursor-pointer hover:-translate-y-[1px] hover:translate-x-[1.8px]  transition ease-in-out duration-300 group"
+        className="border border-zinc-900 bg-nile-blue-800 px-3 lg:px-5 w-full max-w-md lg:max-w-3xl rounded-lg shadow-shape cursor-pointer hover:-translate-y-[1px] hover:translate-x-[1.8px]  transition ease-in-out duration-300 group"
       >
         <div className="flex items-center gap-5 py-2.5">
           <div className="flex items-center justify-center h-24 px-3 border border-mint-600 rounded-xl shadow-shape min-w-30">
