507306ec2dcf4b6a34057906298f040105c4891c
TamaraRibeiro <tamara.fl@hotmail.com>
2025-02-23 20:18:09 -0300

    :sparkles: add search and paging functions

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,3 @@
-import { BiSearchAlt } from "react-icons/bi";
 import {
   LuChevronLeft,
   LuChevronRight,
@@ -8,10 +7,13 @@ import {
 import { useEffect, useState } from "react";
 import CardList from "./card-list";
 import axios from "axios";
-import { FetchPokemonProps } from "../@types/types";
+import { FetchPokemonProps, Species } from "../@types/types";
 
 export default function Home() {
   const [characters, setCharacters] = useState<FetchPokemonProps>();
+  const [allCharacters, setAllCharacters] = useState<FetchPokemonProps>();
+  const [dataSearch, setDataSearch] = useState<Species[]>([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function getCharacters() {
@@ -24,6 +26,87 @@ export default function Home() {
     getCharacters();
   }, []);
 
+  async function findPokemonByName(searchValue: string) {
+    if (searchValue === "") setDataSearch([]);
+
+    const result = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?limit=${characters?.count}&offset=0`
+    );
+    setAllCharacters(result.data);
+
+    if (searchValue !== "") {
+      const filteredCharacters = allCharacters?.results.filter((character) =>
+        character.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+
+      if (filteredCharacters) {
+        setDataSearch(filteredCharacters);
+      }
+    }
+
+    console.log(dataSearch);
+  }
+
+  function goToNextPage() {
+    if (characters?.next) {
+      axios
+        .get(characters?.next)
+        .then((response) => {
+          setCharacters(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+        setPage(page + 1);
+    }
+  }
+
+  function goToPreviousPage() {
+    if (characters?.previous) {
+      axios
+        .get(characters?.previous)
+        .then((response) => {
+          setCharacters(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      setPage(page - 1);
+    }
+  }
+
+  function goToFirstPage() {
+    if (characters) {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0")
+        .then((response) => {
+          setCharacters(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+        setPage(1);
+    }
+  }
+
+  function goToLastPage() {
+    if (characters) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${characters.count - (characters.count%10)}`)
+        .then((response) => {
+          setCharacters(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+        setPage(Math.ceil(characters.count/10));
+    }
+  }
+
   return (
     <>
       <div className="flex justify-center items-center px-6 py-10 h-full tracking-wide">
@@ -33,40 +116,46 @@ export default function Home() {
           </h1>
           <div className="flex gap-2 w-full justify-center">
             <input
+              onChange={(e) => findPokemonByName(e.target.value)}
               type="text"
               placeholder="Search for a Pokemon"
               className="px-1.5 py-0.5 border border-mint-500 rounded-lg w-full text-sm max-w-96 bg-opacity-20 outline-transparent focus:outline-mint-400 bg-zinc-800/40 placeholder:text-zinc-400/80"
             />
-            <button className="flex items-center gap-1.5 rounded-lg text-zinc-800 border border-mint-800 px-2 py-0.5 bg-mint-500 hover:bg-mint-400 transition ease-in-out duration-300 hover:scale-105 cursor-pointer">
+            {/* <button
+              className="flex items-center gap-1.5 rounded-lg text-zinc-800 border border-mint-800 px-2 py-0.5 bg-mint-500 hover:bg-mint-400 transition ease-in-out duration-300 hover:scale-105 cursor-pointer"
+            >
               Search
               <BiSearchAlt />
-            </button>
+            </button> */}
           </div>
           <div className="rounded-3xl px-6 py-8 w-full flex justify-center items-center flex-col space-y-4 shadow-lg">
             <div className="w-full flex flex-col items-center lg:grid lg:grid-cols-2 mx-auto gap-6">
-              {characters &&
-                characters.results.map((character, index) => (
-                  <CardList key={index} pokemonCharacter={character} />
-                ))}
+              {dataSearch.length > 0
+                ? dataSearch?.map((character, index) => (
+                    <CardList key={index} pokemonCharacter={character} />
+                  ))
+                : characters?.results.map((character, index) => (
+                    <CardList key={index} pokemonCharacter={character} />
+                  ))}
             </div>
 
-            <div className="mt-8 flex flex-col items-center gap-3">
+            <div className="mt-8 flex flex-col items-center gap-5">
               <div className="flex gap-1.5">
-                <div className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer">
+                <button disabled={page === 1} onClick={goToFirstPage} className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer disabled:cursor-not-allowed disabled:hover:scale-100 disabled:opacity-60">
                   <LuChevronsLeft size={20} />
-                </div>
-                <div className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer">
+                </button>
+                <button disabled={page === 1} onClick={goToPreviousPage} className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer disabled:cursor-not-allowed disabled:hover:scale-100 disabled:opacity-60">
                   <LuChevronLeft size={20} />
-                </div>
-                <div className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer">
+                </button>
+                <button disabled={page === (characters && Math.ceil(characters?.count/10))} onClick={goToNextPage} className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer disabled:cursor-not-allowed disabled:hover:scale-100 disabled:opacity-60">
                   <LuChevronRight size={20} />
-                </div>
-                <div className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer">
+                </button>
+                <button disabled={page === (characters && Math.ceil(characters?.count/10))} onClick={goToLastPage} className="border p-0.5 border-zinc-700/10 shadow-shape rounded-sm hover:scale-110 transition ease-in-out duration-200 cursor-pointer disabled:cursor-not-allowed disabled:hover:scale-100 disabled:opacity-60">
                   <LuChevronsRight size={20} />
-                </div>
+                </button>
               </div>
               <div>
-                <p className="opacity-70">Page 1 of 10</p>
+                <p className="opacity-70">Page {page} of {characters && Math.ceil(characters.count/10)}</p>
               </div>
             </div>
           </div>
